1dded8357095ea960d89e82627055d6096f63672
khazarglzd <khazarguluzade@gmail.com>
2024-12-04 20:17:35 +0400

    adding TypeCard component & displaying datas

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
-import { getPokeManiaNumber } from '../utils'
+import { getPokeManiaNumber, getFullPokeManiaNumber } from '../utils'
+import TypeCard from "./TypeCard"
 
 export default function PokeCard(props) {
     const { selectedPokemon } = props
     const [data, setData] = useState(null)
     const [loading, setLoading] = useState(false)
+
+    const { name, height, abilities, stats, types, moves, sprites } = data || {}
 
     useEffect(() => {
         // if loading, exit logic
@@ -50,7 +53,29 @@ export default function PokeCard(props) {
 
     }, [selectedPokemon])
 
+    if (loading || !data) {
+        return (
+            <div>
+                <h4>Loading...</h4>
+            </div>
+        )
+    }
+
     return (
-        <div>PokeCard</div>
+        <div className='poke-card'>
+            <div >
+                <h4>#{getFullPokeManiaNumber(selectedPokemon)}</h4>
+                <h2>{name}</h2>
+            </div>
+            <div className='type-container'>
+                {types.map((typeObj, typeIndex) => {
+                    return (
+                        <TypeCard key={typeIndex} type={typeObj?.type?.name} />
+                    )
+                })}
+            </div>
+            <img className='default-img' src={'/pokemon/' + getFullPokeManiaNumber(selectedPokemon) + '.png'} alt={`${name}-large-img`} />
+
+        </div>
     )
 }
